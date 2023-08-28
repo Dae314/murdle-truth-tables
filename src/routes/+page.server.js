745c2d0953543781a7ac774e96c0fedce98e99aa
Dae314 <discord@dae314>
2023-08-27 20:03:08 -1000

    Add base path to initial redirect

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,8 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
+
+
 
 export function load() {
-	throw redirect(307, '/step1');
+	throw redirect(307, `${base}/step1`);
 }
