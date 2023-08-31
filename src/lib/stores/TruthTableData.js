236dbef105f756cfb9ccd5fdd5884350636b77a2
Dae314 <discord@dae314>
2023-08-30 20:03:42 -1000

    Swap order of weapons and locations categories

--- src/lib/stores/TruthTableData.js
+++ src/lib/stores/TruthTableData.js
@@ -4,8 +4,8 @@ import MurdleCategory from '$lib/classes/MurdleCategory';
 const TruthTableData = writable({
 	murdle_categories: [
 		new MurdleCategory({id: 0, name: 'Suspects', active: true}),
-		new MurdleCategory({id: 1, name: 'Locations', active: true}),
-		new MurdleCategory({id: 2, name: 'Weapons', active: true}),
+		new MurdleCategory({id: 1, name: 'Weapons', active: true}),
+		new MurdleCategory({id: 2, name: 'Locations', active: true}),
 		new MurdleCategory({id: 3, name: 'Motives', active: false}),
 	],
 	truth_tables: [],
